c3330b3153d948a2db68fb2aaa98b0709462f340
Harry <47131802+parkhwyy@users.noreply.github.com>
2020-05-01 00:41:44 -0400

    Add files via upload

---
+++ Final/final_q3/Buffer.java
@@ -0,0 +1,38 @@
+import java.io.DataInputStream;
+
+public class Buffer {
+
+		private String line = "";
+		private int position = 0;
+		private DataInputStream inStream;
+
+		public Buffer(DataInputStream i) {
+			this.inStream = i;
+		}
+
+		@SuppressWarnings("deprecation")
+		public char getChar() {
+
+			position++;
+
+			if (position >= line.length()) {
+
+				try {
+					line = inStream.readLine();
+				} catch (Exception e) {
+					System.err.println("Invalid");
+					System.exit(1);
+				}
+
+				if (line == null)
+					System.exit(0);
+
+				position = 0;
+				line = line + "\n";
+			}
+
+			return line.charAt(position);
+		}
+
+	}
+ 
---
+++ Final/final_q3/Token.java
@@ -0,0 +1,37 @@
+class Token {  
+ 
+    public static final int SEMICOLON = 0; 
+    public static final int COMMA = 1;
+    public static final int NOT_EQ = 2;     
+    public static final int ADD_OP = 3;   
+    public static final int SUB_OP = 4;   
+    public static final int MULT_OP = 5;   
+    public static final int DIV_OP = 6;   
+    public static final int ASSIGN_OP = 7;   
+    public static final int EQ_OP = 8;
+    public static final int GREATER_OP = 9;
+    public static final int GREATER_EQ_OP = 10;
+    public static final int LESSER_OP = 11;
+    public static final int LESSER_EQ_OP = 12;
+    public static final int LEFT_PAREN= 13;   
+    public static final int RIGHT_PAREN= 14;   
+    public static final int LEFT_BRACE= 15;   
+    public static final int RIGHT_BRACE= 16;   
+    public static final int ID = 17;   
+    public static final int INT_LIT  = 18;
+    public static final int KEY_IF = 19;
+    public static final int KEY_INT = 20;
+    public static final int KEY_ELSE = 21;
+    public static final int KEY_WHILE = 22;
+    
+    private static String[] lexemes = {   
+        ";", ",", "!=", "+", "-", "*", "/", "=", ">", "<", "(", ")", "{", "}", "id", "int_lit", "if", "int", "else", "while", "end"
+    };   
+    
+    public static String toString (int i) {   
+        if (i < 0 || i > KEY_WHILE)   
+        return "";   
+        else return lexemes[i];   
+    }
+
+} 
